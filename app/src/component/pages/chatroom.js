57a70c953de44b89aad3bab6f34332422950ac6a
Hauw <hauwricky@gmail.com>
2019-05-08 18:36:31 +0700

    commit socket

--- app/src/component/pages/chatroom.js
+++ app/src/component/pages/chatroom.js
@@ -3,7 +3,7 @@ import {InputGroup,Form,Button} from 'react-bootstrap';
 import socketIOClient from 'socket.io-client';
 import Moment from 'moment';
 
-const endpoint = "/bpmndiagram";
+const endpoint = "https://realtime-e-bpmn.herokuapp.com/";
 const socket = socketIOClient(endpoint);
 
 export class chatroom extends Component {
