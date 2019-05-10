777928f38e744d71c107866c3962a5b07c40542a
Hauw <hauwricky@gmail.com>
2019-05-10 11:01:45 +0700

    socket

--- app/src/component/pages/BpmnModelerComponent.js
+++ app/src/component/pages/BpmnModelerComponent.js
@@ -18,7 +18,7 @@ import canvg from 'canvg-browser';
 
 import socketIOClient from 'socket.io-client';
 
-const endpoint = "http://realtime-e-bpmn.herokuapp.com/bpmndiagram";
+const endpoint = "https://realtime-e-bpmn.herokuapp.com/bpmndiagram";
 const socket = socketIOClient(endpoint);
 
 // const endpoint = "http://localhost:3000/bpmndiagram";
--- app/src/component/pages/chatroom.js
+++ app/src/component/pages/chatroom.js
@@ -3,7 +3,7 @@ import {InputGroup,Form,Button} from 'react-bootstrap';
 import socketIOClient from 'socket.io-client';
 import Moment from 'moment';
 
-const endpoint = "http://realtime-e-bpmn.herokuapp.com/bpmndiagram";
+const endpoint = "https://realtime-e-bpmn.herokuapp.com/bpmndiagram";
 const socket = socketIOClient(endpoint);
 
 // const endpoint = "/bpmndiagram";
