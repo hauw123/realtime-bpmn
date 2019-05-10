e6f716b1178994a449397d865d5938be92eaccc7
Hauw <hauwricky@gmail.com>
2019-05-10 13:52:30 +0700

    socket

--- app/src/component/pages/chatroom.js
+++ app/src/component/pages/chatroom.js
@@ -3,7 +3,7 @@ import {InputGroup,Form,Button} from 'react-bootstrap';
 import socketIOClient from 'socket.io-client';
 import Moment from 'moment';
 
-const endpoint = "https://realtime-e-bpmn.herokuapp.com/";
+const endpoint = "https://realtime-e-bpmn.herokuapp.com/bpmndiagram";
 const socket = socketIOClient(endpoint);
 
 // const endpoint = "/bpmndiagram";
